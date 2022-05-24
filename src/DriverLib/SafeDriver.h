0d9567c314b9a481d1e44ef003afd84f9b64636f
wlapacz <01169203@pw.edu.pl>
2022-05-24 19:05:07 +0200

    safe_driver model

---
+++ src/DriverLib/SafeDriver.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include "Driver.h"
+
+class SafeDriver : public Driver
+{
+protected:
+    void calculateSpeed() noexcept;
+public:
+    SafeDriver() noexcept;
+    SafeDriver(std::vector<int> position, char direction, std::vector<int> destination, std::vector<int> distances, std::vector<int> attributes) noexcept;
+    void updatePosition(std::vector<int> new_position, char new_direction, std::vector<int> new_distances, std::vector<int> new_attributes) noexcept;
+};
