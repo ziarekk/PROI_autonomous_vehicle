44d756131e9125c670eab6a86ec0a9cfd233f221
wlapacz <01169203@pw.edu.pl>
2022-05-24 19:03:17 +0200

    car model v2

--- src/DriverLib/Car.h
+++ src/DriverLib/Car.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 #include <vector>
 
@@ -6,10 +7,16 @@ using namespace std;
 class Car
 {
 private:
+    vector<int> position;
+    vector<int> destination;
     vector<int> attributes;
+    char direction = 's';
 public:
     vector<int> getAttributes() const noexcept;
-    void turnLeft() noexcept;
-    void turnRight() noexcept;
-    void accelerate() noexcept;
+    void turnLeft() noexcept {}
+    void turnRight() noexcept {}
+    void accelerate() noexcept {}
+    char getDirection() const noexcept {
+        return this->direction;
+    }
 };
