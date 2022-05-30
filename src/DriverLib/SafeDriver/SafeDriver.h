51d7c3bbdb208028c4463808f5a3d5f3c1118207
wlapacz <01169203@pw.edu.pl>
2022-05-30 09:11:06 +0200

    working

--- src/DriverLib/SafeDriver/SafeDriver.h
+++ src/DriverLib/SafeDriver/SafeDriver.h
@@ -1,15 +1,15 @@
-// #pragma once
-// #include <iostream>
-// #include <vector>
-// #include "../BaseDriver/Driver.h"
-// #include "../MapLib/Position.h"
+#pragma once
+#include <iostream>
+#include <vector>
+#include "../BaseDriver/Driver.h"
+#include "../MapLib/Position.h"
 
-// class SafeDriver : public Driver
-// {
-// protected:
-//     void calculateSpeed() noexcept;
-// public:
-//     SafeDriver() noexcept;
-//     SafeDriver(Position position, int maxSpeed, char direction, Position destination, std::vector<int> distances, std::vector<int> attributes) noexcept;
-//     void updatePosition(Car &car) noexcept;
-// };
+class SafeDriver : public Driver
+{
+protected:
+    void calculateSpeed(Car &car) noexcept;
+public:
+    SafeDriver() noexcept;
+    SafeDriver(Car &car, Position destination) noexcept;
+    void updatePosition(Car &car) noexcept;
+};
