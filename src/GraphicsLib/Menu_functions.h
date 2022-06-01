f9b0a24bf7adc4f12f1d16d0ddec5eb187d5e9c3
kziarek <01169264@pw.edu.pl>
2022-06-01 20:23:51 +0200

    Moved Menu Functions to separate file
    Moved Menu Functions to separate file

---
+++ src/GraphicsLib/Menu_functions.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <iostream>
+#include "../CarLib/Car.h"
+#include "data_loader.h"
+
+int takeMap() {
+    int mapNumber = 0;
+    while (mapNumber > 3 || mapNumber < 1) {
+        std::cout << "There are 3 available maps: map_1, map_2, map_3" << std::endl;
+        std::cout << "Type number of the map: ";
+        std::cin >> mapNumber;
+    }
+    return mapNumber;
+}
+
+char takeDriver() {
+    char driverType = 'X';
+    while(driverType!='S' && driverType!='I'){
+        std::cout<<"There are 2 available drivers: S - stupid, I - Intelligent"<<std::endl;
+        std::cout<<"Type the letter of the driver: ";
+        std::cin>>driverType;
+    }
+    return driverType;
+}
+
+std::string chooseMap(int mType)
+{
+    if(mType== 1)
+        return "map_1.txt";
+    else if(mType == 2)
+        return "map_2.txt";
+    else
+        return "map_3.txt";
+}
+
+Position takeStartingPoint(Map& map) {
+    Position startingPoint;
+    do {
+        std::cout << "Now choose starting point!" << std::endl << "X = ";
+        std::cin >> startingPoint.x;
+        std::cout << "Y = ";
+        std::cin >> startingPoint.y;
+    } while (map(startingPoint).isBarrier);
+    return startingPoint;
+}
+
+Position takeDestinationPoint(Map& map){
+    Position destinationPoint;
+    do{
+        std::cout<<"Now choose destination point!"<<std::endl<<"X = ";
+        std::cin>>destinationPoint.x;
+        std::cout<<"Y = ";
+        std::cin>>destinationPoint.y;
+    }while(map(destinationPoint).isBarrier);
+    return destinationPoint;
+}
+
+void displayCurrentState(Car& car, Position destinationPoint){
+    std::cout<<std::endl<<"Current Position: ("<<car.getLocation().x<<","<< car.getLocation().y<<")   ";
+    std::cout<<std::endl<<"Destination: ("<<destinationPoint.x<<","<< destinationPoint.y<<")   ";
+    std::cout<<std::endl;
+    std::cout<<std::endl<<"Road quality: "<<car.getSurfaceCondition()<<"   ";
+    std::cout<<std::endl<<"Road temperature: "<<car.getTemperature()<<"   ";
+    std::cout<<std::endl<<"Road humidity: "<<car.getHumidityInfo()<<"   ";
+    std::cout<<std::endl;
+    std::cout<<std::endl<<"Wall distances: "<<std::endl;
+    std::cout<<"Front - "<<car.getRadarInfo()[0]<<";  Left - "<<car.getRadarInfo()[1];
+    std::cout<<";  Right - "<<car.getRadarInfo()[2]<<"     "<<std::endl;
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,21 @@
-#include "CarLib/Car.h"
-#include "DriverLib/BaseDriver/Driver.h"
-#include "GraphicsLib/data_loader.h"
+#include "GraphicsLib/Menu_functions.h"
 #include "GraphicsLib/Displays/ConsoleDisplay.h"
+#include "../DriverLib/BaseDriver/Driver.h"
 #include "DriverLib/StupidDriver/StupidDriver.h"
 
-int takeMap(ConsoleDisplay& display, Drawing& drawing) {
-    int mapNumber = 0;
-    while (mapNumber > 3 || mapNumber < 1) {
-        std::cout << "There are 3 available maps: map_1, map_2, map_3" << std::endl;
-        std::cout << "Type number of the map: ";
-        std::cin >> mapNumber;
-    }
-    return mapNumber;
-}
-
-char takeDriver(ConsoleDisplay& display, Drawing& drawing) {
-    char driverType = 'X';
-    while(driverType!='S' && driverType!='I'){
-        std::cout<<"There are 2 available drivers: S - stupid, I - Intelligent"<<std::endl;
-        std::cout<<"Type the letter of the driver: ";
-        std::cin>>driverType;
-    }
-}
-
-std::string chooseMap(int mType)
-    {
-    if(mType== 1)
-        return "map_1.txt";
-    else if(mType == 2)
-        return "map_2.txt";
-    else
-        return "map_3.txt";
-    }
-
-Position takeStartingPoint(ConsoleDisplay& display, Drawing& drawing, Map& map) {
-    Position startingPoint;
-    do {
-        std::cout << "Now choose starting point!" << std::endl << "X = ";
-        std::cin >> startingPoint.x;
-        std::cout << "Y = ";
-        std::cin >> startingPoint.y;
-    } while (map(startingPoint).isBarrier);
-    return startingPoint;
-}
-
-Position takeDestinationPoint(ConsoleDisplay& display, Drawing& drawing, Map& map){
-    Position destinationPoint;
-    do{
-        std::cout<<"Now choose destination point!"<<std::endl<<"X = ";
-        std::cin>>destinationPoint.x;
-        std::cout<<"Y = ";
-        std::cin>>destinationPoint.y;
-    }while(map(destinationPoint).isBarrier);
-    return destinationPoint;
-}
 
 int main() {
     Drawing drawUI;
     ConsoleDisplay cmdDisplay;
 
-    int mapNumber = takeMap(cmdDisplay, drawUI);
-    char driverType= takeDriver(cmdDisplay, drawUI);
+    int mapNumber = takeMap();
+    char driverType= takeDriver();
     std::string mapName = chooseMap(mapNumber);
 
     Map map(mapName);
 
-    Position startingPoint = takeStartingPoint(cmdDisplay, drawUI, map);
-    Position destinationPoint = takeDestinationPoint(cmdDisplay, drawUI, map);
+    Position startingPoint = takeStartingPoint(map);
+    Position destinationPoint = takeDestinationPoint(map);
 
 
     Car car(startingPoint, map);
@@ -74,14 +23,13 @@ int main() {
 
     drawUI += std::make_unique<clearConsole>(clearConsole(70, 70, ' ', cmdDisplay));
     drawUI.display();
-    //load data from map&car and display it
+
     Loader.load_data();
     drawUI.display();
 
     std::string str;
 
-    Position footer(0, map.getFieldContainer().size());
-    Position footer2(0, map.getFieldContainer().size()+12);
+    Position footer(0, int(map.getFieldContainer().size()));
 
     if(driverType == 'S')
         {
@@ -91,17 +39,7 @@ int main() {
             drawUI += std::make_unique<DisplayTile>(DisplayTile(footer, cmdDisplay, ' '));
             drawUI.display();
 
-            std::cout<<std::endl<<"Current Position: ("<<car.getLocation().x<<","<< car.getLocation().y<<")   ";
-            std::cout<<std::endl<<"Destination: ("<<destinationPoint.x<<","<< destinationPoint.y<<")   ";
-            std::cout<<std::endl;
-            std::cout<<std::endl<<"Road quality: "<<car.getSurfaceCondition();
-            std::cout<<std::endl<<"Road temperature: "<<car.getTemperature();
-            std::cout<<std::endl<<"Road humidity: "<<car.getHumidityInfo();
-            std::cout<<std::endl;
-            std::cout<<std::endl<<"Wall distances: "<<std::endl;
-            std::cout<<"Front - "<<car.getRadarInfo()[0]<<";  Left - "<<car.getRadarInfo()[1];
-            std::cout<<";  Right - "<<car.getRadarInfo()[2]<<std::endl;
-
+            displayCurrentState(car, destinationPoint);
 
             driver.navigate(car);
             car.move();
@@ -110,9 +48,9 @@ int main() {
             Sleep(1000);
             }
         Loader.load_data();
-        drawUI += std::make_unique<DisplayTile>(DisplayTile(footer2, cmdDisplay, ' '));
+        drawUI += std::make_unique<DisplayTile>(DisplayTile(footer, cmdDisplay, ' '));
         drawUI.display();
-        std::cout<<std::endl;
+        displayCurrentState(car, destinationPoint);
         }
 
     else {
@@ -121,17 +59,8 @@ int main() {
             Loader.load_data();
             drawUI += std::make_unique<DisplayTile>(DisplayTile(footer, cmdDisplay, ' '));
             drawUI.display();
-            std::cout<<std::endl<<"Current Position: ("<<car.getLocation().x<<","<< car.getLocation().y<<")    ";
-            std::cout<<std::endl<<"Destination: ("<<destinationPoint.x<<","<< destinationPoint.y<<")   ";
-            std::cout<<std::endl;
-            std::cout<<std::endl<<"Road quality: "<<car.getSurfaceCondition()<<"          ";
-            std::cout<<std::endl<<"Road temperature: "<<car.getTemperature()<<"          ";
-            std::cout<<std::endl<<"Road humidity: "<<car.getHumidityInfo()<<"          ";
-            std::cout<<std::endl;
-            std::cout<<std::endl<<"Wall distances: "<<std::endl;
-            std::cout<<"Front - "<<car.getRadarInfo()[0]<<";  Left - "<<car.getRadarInfo()[1];
-            std::cout<<";  Right - "<<car.getRadarInfo()[2]<<"          "<<std::endl;
 
+            displayCurrentState(car, destinationPoint);
 
             driver.navigate(car);
             car.move();
@@ -140,9 +69,9 @@ int main() {
             Sleep(1000);
         }
         Loader.load_data();
-        drawUI += std::make_unique<DisplayTile>(DisplayTile(footer2, cmdDisplay, ' '));
+        drawUI += std::make_unique<DisplayTile>(DisplayTile(footer, cmdDisplay, ' '));
         drawUI.display();
-        std::cout<<std::endl;
+        displayCurrentState(car, destinationPoint);
     }
 
     system("pause");
