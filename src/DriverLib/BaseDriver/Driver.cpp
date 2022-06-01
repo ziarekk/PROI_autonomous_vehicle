434c8db77a5e714537641e4afcc0749e8ad4b840
wlapacz <01169203@pw.edu.pl>
2022-06-01 18:58:05 +0200

    random in avoidWall

--- src/DriverLib/BaseDriver/Driver.cpp
+++ src/DriverLib/BaseDriver/Driver.cpp
@@ -9,6 +9,28 @@ Position Driver::getDestination() const noexcept {
     return this->destination;
 }
 
+void Driver::avoidWall(Car &car) noexcept {
+    int left = this->wall_distances[1];
+    int right = this->wall_distances[2];
+    int v = rand() % 2;
+    if (v == 0) {
+        if (left > 1) {
+            car.turnLeft();
+        }
+        else{
+            car.turnRight();
+        }
+    }
+    else {
+        if (right > 1) {
+            car.turnRight();
+        }
+        else{
+            car.turnLeft();
+        }
+    }
+}
+
 void Driver::navigate(Car &car) noexcept {
     // destination reached
     if (position == destination) {
